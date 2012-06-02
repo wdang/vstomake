74bad5de2b89bb2a9f83f3bbf04253ace467872c
wdang <williamvandang@gmail.com>
2012-06-01 20:02:29 -0600

    Replace return types to better represent underlying property values

--- src/vcclcompilertool.cc
+++ src/vcclcompilertool.cc
@@ -14,88 +14,125 @@
 #include "precompiled.h"
 #include "vcclcompilertool.h"
 
-#define VCCLCOMPILERTOOL_IMPL X(AdditionalIncludeDirectories) \
-  X(AdditionalOptions) \
-  X(AdditionalUsingDirectories) \
-  X(AssemblerListingLocation) \
-  X(AssemblerOutput) \
-  X(BasicRuntimeChecks) \
-  X(BrowseInformation) \
-  X(BrowseInformationFile) \
-  X(BufferSecurityCheck) \
-  X(CallingConvention) \
-  X(CompileAs) \
-  X(CompileAsManaged) \
-  X(CompileOnly) \
-  X(DebugInformationFormat) \
-  X(DefaultCharIsUnsigned) \
-  X(Detect64BitPortabilityProblems) \
-  X(DisableLanguageExtensions) \
-  X(DisableSpecificWarnings) \
-  X(EnableEnhancedInstructionSet) \
-  X(EnableFiberSafeOptimizations) \
-  X(EnableFunctionLevelLinking) \
-  X(EnableIntrinsicFunctions) \
-  X(EnablePREfast) \
-  X(ErrorReporting) \
-  X(ExceptionHandling) \
-  X(ExecutionBucket) \
-  X(ExpandAttributedSource) \
-  X(FavorSizeOrSpeed) \
-  X(FloatingPointExceptions) \
-  X(floatingPointModel) \
-  X(ForceConformanceInForLoopScope) \
-  X(ForcedIncludeFiles) \
-  X(ForcedUsingFiles) \
-  X(FullIncludePath) \
-  X(GeneratePreprocessedFile) \
-  X(GenerateXMLDocumentationFiles) \
-  X(IgnoreStandardIncludePath) \
-  X(InlineFunctionExpansion) \
-  X(KeepComments) \
-  X(MinimalRebuild) \
-  X(ObjectFile) \
-  X(OmitDefaultLibName) \
-  X(OmitFramePointers) \
-  X(OpenMP) \
-  X(Optimization) \
-  X(PrecompiledHeaderFile) \
-  X(PrecompiledHeaderThrough) \
-  X(PreprocessorDefinitions) \
-  X(ProgramDataBaseFileName) \
-  X(RuntimeLibrary) \
-  X(RuntimeTypeInfo) \
-  X(ShowIncludes) \
-  X(SmallerTypeCheck) \
-  X(StringPooling) \
-  X(StructMemberAlignment) \
-  X(SuppressStartupBanner) \
-  X(ToolKind) \
-  X(toolName) \
-  X(ToolPath) \
-  X(TreatWChar_tAsBuiltInType) \
-  X(UndefineAllPreprocessorDefinitions) \
-  X(UndefinePreprocessorDefinitions) \
-  X(UseFullPaths) \
-  X(UsePrecompiledHeader) \
-  X(UseUnicodeResponseFiles) \
-  X(VCProjectEngine) \
-  X(WarnAsError) \
-  X(WarningLevel) \
-  X(WholeProgramOptimization) \
-  X(XMLDocumentationFileName)
-
-
-#define X(NAME) const char* VCCLCompilerTool::NAME() const { \
-    auto iter = properties.find(# NAME); \
-    return iter == properties.end() ? "" : iter->second.c_str(); \
-}
-VCCLCOMPILERTOOL_IMPL
-#undef X
-
+// VCCLCompilerTool constructor implementation
 VCCLCompilerTool::VCCLCompilerTool(const VCProject::Configuration& configuration) {
   auto iter = configuration.properties.find("VCCLCompilerTool");
   if (iter != configuration.properties.end()) {
     properties = iter->second;
   }
 }
+
+
+VCCLCompilerTool::Enum GetEnum(const std::string&){return VCCLCompilerTool::Enum_Unknown;}
+// Implemented via excessive X-macros ;)
+// Implemention for members returning a VCCLCompilerTool::Enum 
+#define VCCLCOMPILERTOOL_ENUM_ACCESSORS X(AssemblerOutput)\
+X(BasicRuntimeChecks)\
+X(BrowseInformation)\
+X(CallingConvention)\
+X(CompileAs)\
+X(CompileAsManaged)\
+X(DebugInformationFormat)\
+X(EnableEnhancedInstructionSet)\
+X(ErrorReporting)\
+X(ExceptionHandling)\
+X(FavorSizeOrSpeed)\
+X(GeneratePreprocessedFile)\
+X(InlineFunctionExpansion)\
+X(Optimization)\
+X(RuntimeLibrary)\
+X(StructMemberAlignment)\
+X(UsePrecompiledHeader)\
+X(WarningLevel)\
+X(floatingPointModel)
+
+#define X(NAME) VCCLCompilerTool::Enum VCCLCompilerTool::NAME() const {\
+  auto iter = properties.find(#NAME);\
+  VCCLCompilerTool::Enum rv;\
+  if(iter != properties.end()){\
+     rv = GetEnum(iter->second);\
+  }else{\
+    rv = Enum_Unknown;\
+  }\
+  return rv;\
+}
+VCCLCOMPILERTOOL_ENUM_ACCESSORS
+#undef X
+
+
+
+// Implemention for members returning a bool
+#define VCCLCOMPILERTOOL_BOOL_ACCESSORS X(BufferSecurityCheck)\
+X(CompileOnly)\
+X(DefaultCharIsUnsigned)\
+X(Detect64BitPortabilityProblems)\
+X(DisableLanguageExtensions)\
+X(EnableFiberSafeOptimizations)\
+X(EnableFunctionLevelLinking)\
+X(EnableIntrinsicFunctions)\
+X(EnablePREfast)\
+X(ExpandAttributedSource)\
+X(FloatingPointExceptions)\
+X(ForceConformanceInForLoopScope)\
+X(GenerateXMLDocumentationFiles)\
+X(IgnoreStandardIncludePath)\
+X(KeepComments)\
+X(MinimalRebuild)\
+X(OmitFramePointers)\
+X(RuntimeTypeInfo)\
+X(ShowIncludes)\
+X(SmallerTypeCheck)\
+X(StringPooling)\
+X(TreatWChar_tAsBuiltInType)\
+X(UndefineAllPreprocessorDefinitions)\
+X(UseUnicodeResponseFiles)\
+X(WarnAsError)\
+X(WholeProgramOptimization)
+
+#define X(NAME) bool VCCLCompilerTool::NAME() const {\
+  auto iter = properties.find(#NAME);\
+  bool rv = false;\
+  if(iter != properties.end()){\
+    std::string value(iter->second);\
+    std::transform(value.begin(),value.end(),value.begin(),tolower);\
+    rv = (value.find("true") != std::string::npos);\
+  }\
+  return rv;\
+}
+VCCLCOMPILERTOOL_BOOL_ACCESSORS
+#undef X
+
+
+
+// Implemention for members returning a string
+#define VCCLCOMPILERTOOL_STRING_ACCESSORS X(AdditionalIncludeDirectories)\
+X(AdditionalOptions)\
+X(AdditionalUsingDirectories)\
+X(AssemblerListingLocation)\
+X(BrowseInformationFile)\
+X(DisableSpecificWarnings)\
+X(ExecutionBucket)\
+X(ForcedIncludeFiles)\
+X(ForcedUsingFiles)\
+X(FullIncludePath)\
+X(ObjectFile)\
+X(OmitDefaultLibName)\
+X(OpenMP)\
+X(PrecompiledHeaderFile)\
+X(PrecompiledHeaderThrough)\
+X(PreprocessorDefinitions)\
+X(ProgramDataBaseFileName)\
+X(SuppressStartupBanner)\
+X(ToolKind)\
+X(ToolPath)\
+X(UndefinePreprocessorDefinitions)\
+X(UseFullPaths)\
+X(VCProjectEngine)\
+X(XMLDocumentationFileName)\
+X(toolName)
+#define X(NAME) const char* VCCLCompilerTool::NAME() const { \
+    auto iter = properties.find(# NAME); \
+    return iter == properties.end() ? "" : iter->second.c_str(); \
+}
+VCCLCOMPILERTOOL_STRING_ACCESSORS
+#undef X
--- src/vcclcompilertool.h
+++ src/vcclcompilertool.h
@@ -15,82 +15,137 @@
 #include "vcproject.h"
 
 // VCCLCompilerTool is for convienent access
-// to the VCCLCompilerTool  properties of a project configuration
+// to the VCCLCompilerTool properties of a project configuration.
 struct VCCLCompilerTool {
-
+  
+  // All possible enumeration values
+  // used by VCCLCompilerTool
+  enum Enum {
+    Enum_Unknown,
+    Enum_asmListingNone,
+    Enum_asmListingAssemblyOnly,
+    Enum_asmListingAsmMachineSrc,
+    Enum_asmListingAsmMachine,
+    Enum_asmListingAsmSrc,
+    Enum_runtimeBasicCheckNone,
+    Enum_runtimeCheckStackFrame,
+    Enum_runtimeCheckUninitVariables,
+    Enum_runtimeBasicCheckAll,
+    Enum_brInfoNone,
+    Enum_brAllInfo,
+    Enum_brNoLocalSymbols,
+    Enum_managedNotSet,
+    Enum_managedAssembly,
+    Enum_managedAssemblyPure,
+    Enum_managedAssemblySafe,
+    Enum_managedAssemblyOldSyntax,
+    Enum_cppExceptionHandlingNo,
+    Enum_cppExceptionHandlingYes,
+    Enum_cppExceptionHandlingYesWithSEH,
+    Enum_debugDisabled,
+    Enum_debugOldStyleInfo,
+    Enum_debugEnabled,
+    Enum_debugEditAndContinue,
+    Enum_enhancedInstructionSetTypeNotSet,
+    Enum_enhancedInstructionSetTypeSIMD,
+    Enum_enhancedInstructionSetTypeSIMD2,
+    Enum_optimizeDisabled,
+    Enum_optimizeMinSpace,
+    Enum_optimizeMaxSpeed,
+    Enum_optimizeFull,
+    Enum_optimizeCustom,
+    Enum_pchNone,
+    Enum_pchCreateUsingSpecific,
+    Enum_pchUseUsingSpecific,
+    Enum_preprocessNo,
+    Enum_preprocessYes,
+    Enum_preprocessNoLineNumbers,
+    Enum_rtMultiThreaded,
+    Enum_rtMultiThreadedDebug,
+    Enum_rtMultiThreadedDLL,
+    Enum_rtMultiThreadedDebugDLL,
+    Enum_alignNotSet,
+    Enum_alignSingleByte,
+    Enum_alignTwoBytes,
+    Enum_alignFourBytes,
+    Enum_alignEightBytes,
+    Enum_alignSixteenBytes
+  };
+  
   // Access VCCLCompilerTool properties from the given
   // project configuration
   explicit VCCLCompilerTool(const VCProject::Configuration& configuration);
   
+  // The following accessors return "" to represent an empty value
   const char* AdditionalIncludeDirectories() const;
   const char* AdditionalOptions() const;
   const char* AdditionalUsingDirectories() const;
   const char* AssemblerListingLocation() const;
-  const char* AssemblerOutput() const;
-  const char* BasicRuntimeChecks() const;
-  const char* BrowseInformation() const;
+  Enum AssemblerOutput() const;
+  Enum BasicRuntimeChecks() const;
+  Enum BrowseInformation() const;
   const char* BrowseInformationFile() const;
-  const char* BufferSecurityCheck() const;
-  const char* CallingConvention() const;
-  const char* CompileAs() const;
-  const char* CompileAsManaged() const;
-  const char* CompileOnly() const;
-  const char* DebugInformationFormat() const;
-  const char* DefaultCharIsUnsigned() const;
-  const char* Detect64BitPortabilityProblems() const;
-  const char* DisableLanguageExtensions() const;
+  bool BufferSecurityCheck() const;
+  Enum CallingConvention() const;
+  Enum CompileAs() const;
+  Enum CompileAsManaged() const;
+  bool CompileOnly() const;
+  Enum DebugInformationFormat() const;
+  bool DefaultCharIsUnsigned() const;
+  bool Detect64BitPortabilityProblems() const;
+  bool DisableLanguageExtensions() const;
   const char* DisableSpecificWarnings() const;
-  const char* EnableEnhancedInstructionSet() const;
-  const char* EnableFiberSafeOptimizations() const;
-  const char* EnableFunctionLevelLinking() const;
-  const char* EnableIntrinsicFunctions() const;
-  const char* EnablePREfast() const;
-  const char* ErrorReporting() const;
-  const char* ExceptionHandling() const;
+  Enum EnableEnhancedInstructionSet() const;
+  bool EnableFiberSafeOptimizations() const;
+  bool EnableFunctionLevelLinking() const;
+  bool EnableIntrinsicFunctions() const;
+  bool EnablePREfast() const;
+  Enum ErrorReporting() const;
+  Enum ExceptionHandling() const;
   const char* ExecutionBucket() const;
-  const char* ExpandAttributedSource() const;
-  const char* FavorSizeOrSpeed() const;
-  const char* FloatingPointExceptions() const;
-  const char* floatingPointModel() const;
-  const char* ForceConformanceInForLoopScope() const;
+  bool ExpandAttributedSource() const;
+  Enum FavorSizeOrSpeed() const;
+  bool FloatingPointExceptions() const;
+  Enum floatingPointModel() const;
+  bool ForceConformanceInForLoopScope() const;
   const char* ForcedIncludeFiles() const;
   const char* ForcedUsingFiles() const;
   const char* FullIncludePath() const;
-  const char* GeneratePreprocessedFile() const;
-  const char* GenerateXMLDocumentationFiles() const;
-  const char* IgnoreStandardIncludePath() const;
-  const char* InlineFunctionExpansion() const;
-  const char* KeepComments() const;
-  const char* MinimalRebuild() const;
+  Enum GeneratePreprocessedFile() const;
+  bool GenerateXMLDocumentationFiles() const;
+  bool IgnoreStandardIncludePath() const;
+  Enum InlineFunctionExpansion() const;
+  bool KeepComments() const;
+  bool MinimalRebuild() const;
   const char* ObjectFile() const;
   const char* OmitDefaultLibName() const;
-  const char* OmitFramePointers() const;
+  bool OmitFramePointers() const;
   const char* OpenMP() const;
-  const char* Optimization() const;
+  Enum Optimization() const;
   const char* PrecompiledHeaderFile() const;
   const char* PrecompiledHeaderThrough() const;
   const char* PreprocessorDefinitions() const;
   const char* ProgramDataBaseFileName() const;
-  const char* RuntimeLibrary() const;
-  const char* RuntimeTypeInfo() const;
-  const char* ShowIncludes() const;
-  const char* SmallerTypeCheck() const;
-  const char* StringPooling() const;
-  const char* StructMemberAlignment() const;
+  Enum RuntimeLibrary() const;
+  bool RuntimeTypeInfo() const;
+  bool ShowIncludes() const;
+  bool SmallerTypeCheck() const;
+  bool StringPooling() const;
+  Enum StructMemberAlignment() const;
   const char* SuppressStartupBanner() const;
   const char* ToolKind() const;
   const char* toolName() const;
   const char* ToolPath() const;
-  const char* TreatWChar_tAsBuiltInType() const;
-  const char* UndefineAllPreprocessorDefinitions() const;
+  bool TreatWChar_tAsBuiltInType() const;
+  bool UndefineAllPreprocessorDefinitions() const;
   const char* UndefinePreprocessorDefinitions() const;
   const char* UseFullPaths() const;
-  const char* UsePrecompiledHeader() const;
-  const char* UseUnicodeResponseFiles() const;
+  Enum UsePrecompiledHeader() const;
+  bool UseUnicodeResponseFiles() const;
   const char* VCProjectEngine() const;
-  const char* WarnAsError() const;
-  const char* WarningLevel() const;
-  const char* WholeProgramOptimization() const;
+  bool WarnAsError() const;
+  Enum WarningLevel() const;
+  bool WholeProgramOptimization() const;
   const char* XMLDocumentationFileName() const;
 
  private:
